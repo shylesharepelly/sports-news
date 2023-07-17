4312a3fa5d8aef12cffb2b8630b9ba6c0337f15a
Shylesh <shylesharepelly@gmail.com>
2023-07-17 16:25:33 +0530

    added static favourites

--- src/pages/articles/index.tsx
+++ src/pages/articles/index.tsx
@@ -1,28 +1,51 @@
+import { Link } from 'react-router-dom';
+
 
 function Articles() {
-//   return (
-//     <div>Trending Articles</div>
-//   )
-// }
 
-  const list1 = ['Item 1', 'Item 2', 'Item 3'];
+  const items = [
+    {
+      id: 1,
+      sport:"cricket",
+      title: 'Item 1',
+      details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      date:"Dec 22,2022",
+      image: 'image.jpg',
+    },
+    {
+      id: 2,
+      sport:"cricket",
+      title: 'Item 2',
+      details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      date:"Dec 22,2022",
+      image: 'image.jpg',
+    },
+    // Add more items as needed
+  ];
 
- 
   return (
-    <div className="m-3">
-        <div className="flex flex-col md:flex-row h-screen">
-          <div className="flex-1 ">
-            <ul className="h-full p-4 bg-gray-100">
-              {list1.map((item, index) => (
-                <li key={index} className="border-b border-gray-400 py-2">
-                  {item}
-                </li>
-              ))}
-            </ul>
+    <div className='m-4'>
+      {items.map((item) => (
+        <div key={item.id} className="my-4 p-4 bg-gray-100 rounded-md flex">
+          <div className="flex-1">
+              <div>
+                <h3 className="text-sm  mb-2">{item.sport}</h3>
+                <h3 className="text-xl font-bold mb-2">{item.title}</h3>
+                <p className="text-black-600 mb-2">{item.details}</p>
+              </div>
+              <div className="flex justify-between items-center mb-2">
+                <h3 className="text-xs text-gray-900">{item.date}</h3>
+                <Link to="/" className=" text-black  underline">
+                  Read More...
+                </Link>
+              </div>
           </div>
-          
+            <div className="ml-4">
+              <img src={item.image} alt={item.title} className="w-40 h-40 object-cover rounded" />
+            </div>
         </div>
-      </div>
+      ))}
+    </div>
   );
 }
 
--- src/pages/favourites/index.tsx
+++ src/pages/favourites/index.tsx
@@ -1,22 +1,62 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
+
 
 function Favourites() {
-  const list1 = ['Item 1', 'Item 2', 'Item 3'];
+  const items = [
+    {
+      id: 1,
+      title: 'Item 1',
+      details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+    {
+      id: 2,
+      title: 'Item 2',
+      details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+    // Add more items as needed
+  ];  
+
   return (
-  <div className='m-3'>
-        <div className="flex flex-col md:flex-row h-screen">
-          <div className="flex-1 ">
-            <ul className="h-full p-4 bg-gray-200">
-              {list1.map((item, index) => (
-                <li key={index} className="border-b border-gray-400 py-2">
-                  {item}
-                </li>
-              ))}
-            </ul>
-          </div>
+  // <div className='m-3  bg-gray-200'>
+  //     <h1 className="py-3 px-4 font-bold">Favourites</h1>
+  //       <div className="flex flex-col md:flex-row h-screen">
+  //         <div className="flex-1 ">
+  //           <ul className="h-full p-4">
+  //             {list1.map((item, index) => (
+  //               <li key={index} className="border-b border-gray-400 py-2">
+  //                 {item}
+  //               </li>
+  //             ))}
+  //           </ul>
+  //         </div>
           
+  //       </div> 
+  //     </div>
+
+  <div className='m-4 bg-gray-200'>
+    <h1 className="py-3 px-4 font-bold">Favourites</h1>
+    <div className='m-2'>
+      {items.map((item) => (
+        <div key={item.id} className="my-4 p-4 bg-white rounded-md flex">
+        <div className="flex-1 bg-white flex flex-col justify-center">
+          <div>
+            <h3 className="text-xl font-bold mb-2">{item.title}</h3>
+            <p className="text-black-600 mb-2">{item.details}</p>
+      
+            <div className="flex justify-center">
+              <button className="mt-4 bg-gray-500 w-full hover:bg-gray-800 text-white py-2 px-4 ">
+                <Link to="/" className="text-white font-bold">
+                  Read More
+                </Link>
+              </button>
+            </div>
+          </div>
         </div>
       </div>
+      
+      ))}
+      </div>
+  </div>
 )}
 
 export default Favourites
