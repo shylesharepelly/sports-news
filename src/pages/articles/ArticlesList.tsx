4bd29e9816ff6bb193b4aea694e2e83b26dde09e
Shylesh <shylesharepelly@gmail.com>
2023-08-15 19:02:45 +0530

    added filter icon

--- src/pages/articles/ArticlesList.tsx
+++ src/pages/articles/ArticlesList.tsx
@@ -78,7 +78,10 @@ const ArticlesList: React.FC = () => {
         </div>
 
         <div className="w-full p-4">
+      
+
           <div className="mb-4 flex items-center justify-end">
+          
             <select
               value={selectedFilter}
               onChange={(e) => setSelectedFilter(e.target.value)}
@@ -88,6 +91,12 @@ const ArticlesList: React.FC = () => {
               <option value="date">Date</option>
               <option value="title">Title</option>
             </select>
+            <div className="p-1 bg-slate-400 m-2">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 01-.659 1.591l-5.432 5.432a2.25 2.25 0 00-.659 1.591v2.927a2.25 2.25 0 01-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 00-.659-1.591L3.659 7.409A2.25 2.25 0 013 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0112 3z" />
+    </svg>
+            </div>
+           
           </div>
         </div>
       </div>
