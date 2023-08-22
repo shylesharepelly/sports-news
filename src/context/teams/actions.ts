9d8d04a2feb5fb011be141ece38cf6b277e3421b
Shylesh <shylesharepelly@gmail.com>
2023-08-22 22:28:38 +0530

    added teams context

---
+++ src/context/teams/actions.ts
@@ -0,0 +1,17 @@
+import { API_ENDPOINT } from '../../config/constants';
+
+export const fetchTeams = async (dispatch: any) => {
+  try {
+    dispatch({ type: "FETCH_TEAMS_REQUEST" });
+    const response = await fetch(`${API_ENDPOINT}/teams`, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json'},
+    });
+    const data = await response.json();
+    console.log("data",data)
+    dispatch({ type: "FETCH_TEAMS_SUCCESS", payload: data });
+  } catch (error) {
+    console.log('Error fetching teams:', error);
+    dispatch({ type: "FETCH_TEAMS_FAILURE", payload: 'Unable to load teams' });
+  }
+};
---
+++ src/context/teams/context.tsx
@@ -0,0 +1,26 @@
+import React, { createContext, useContext, useReducer } from "react";
+import { reducer, initialState, TeamsState, TeamsActions } from "./reducer";
+
+const TeamsStateContext = createContext<TeamsState | undefined>(undefined);
+
+export const useTeamsState = () => useContext(TeamsStateContext);
+type TeamsDispatch = React.Dispatch<TeamsActions>;
+const TeamsDispatchContext = createContext<TeamsDispatch | undefined>(undefined);
+
+export const useTeamsDispatch = () => useContext(TeamsDispatchContext);
+
+
+export const TeamsProvider: React.FC<React.PropsWithChildren> = ({
+  children,
+}) => {
+  const [stateTeam, dispatchTeam] = useReducer(reducer, initialState);
+
+  return (
+    <TeamsStateContext.Provider value={stateTeam}>
+      <TeamsDispatchContext.Provider value={dispatchTeam}>
+        {children}
+      </TeamsDispatchContext.Provider>
+    </TeamsStateContext.Provider>
+  );
+};
+
