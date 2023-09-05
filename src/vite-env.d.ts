a8c99dfb393c4c54c003b1d49b7e625b82daf59c
Shylesh <shylesharepelly@gmail.com>
2023-09-05 19:31:54 +0530

    added pwa

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+
+interface ImportMetaEnv {
+    readonly VITE_API_ENDPOINT: string;
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
+  
